98a820048b7491cd1dc4399417e766cc08f37078
cre <cre@chriseliot.com>
2022-12-28 08:18:50 -0500

    IMU calibration

--- smart_car/ImuPlugin.cpp
+++ smart_car/ImuPlugin.cpp
@@ -5,9 +5,10 @@
  *      Author: cre
  */
 
+#include "Arduino.h"
 #include "ImuPlugin.hpp"
 
-ImuPlugin::ImuPlugin () : Plugin(IMU_PLUGIN)
+ImuPlugin::ImuPlugin () : Plugin(IMU_PLUGIN), mpu(0x68)
 {
 }
 
@@ -18,14 +19,186 @@ void ImuPlugin::setup_imu ()
     Wire.write(0x6B);  // PWR_MGMT_1 register
     Wire.write(0);     // set to zero (wakes up the MPU-6050)
     Wire.endTransmission(true);
-    Serial.println("imu setup");
+    mpu.initialize();
+    if (mpu.testConnection())
+    {
+        Serial.println("imu calibration...please wait 15 seconds");
+        // 17:08:16: >...............>...............-1152.00000,   -3917.00000,    1930.00000, 9.00000,    -78.00000,  -17.00000
+        mpu.CalibrateGyro(); // Fine tune after setting offsets with less Loops.
+        mpu.CalibrateAccel(); // Fine tune after setting offsets with less Loops.
+        mpu.PrintActiveOffsets(); // See the results of the Calibration
+
+        // This long calibration is not required.
+        // The built in mpu calibration above does well enough and is much faster.
+        Serial.println("Long imu calibration...please wait 60 seconds");
+        calibrateSensor();
+        Serial.println("imu setup");
+    }
+    else
+    {
+        Serial.println("imu failed");
+    }
+}
+//--------------------------------------------//
+// Read the raw sensor values
+//--------------------------------------------//
+void ImuPlugin::readIMU ()
+{
+    // Using the MotionApps library
+    accX = mpu.getAccelerationX();
+    accY = mpu.getAccelerationY();
+    accZ = mpu.getAccelerationZ();
+    gyroX = mpu.getRotationX();
+    gyroY = mpu.getRotationY();
+    gyroZ = mpu.getRotationZ();
+}
+
+//----------------------------------------------------//
+// Calibrate bias of the accelerometer and gyroscope
+// Sensor needs to be calibrated at each power cycle.
+//----------------------------------------------------//
+void ImuPlugin::calibrateSensor ()
+{
+    // Initialize the offset values
+    setOffsets(-1152.00000, -3917.00000, 1931.00000, 7.00000, -78.00000, -17.00000);
+
+    Serial.println("Reading sensors for first time...");
+    meanSensors_();
+    delay(1000);
+
+    Serial.println("Calculating offsets...");
+    calibrate_();
+    log_mean_values();
+    log_offset_values();
+}
+
+void ImuPlugin::setOffsets (int16_t _ax_offset, int16_t _ay_offset, int16_t _az_offset,
+        int16_t _gx_offset, int16_t _gy_offset, int16_t _gz_offset)
+{
+    ax_offset = _ax_offset;
+    ay_offset = _ay_offset;
+    az_offset = _az_offset;
+    gx_offset = _gx_offset;
+    az_offset = _az_offset;
+    gx_offset = _gx_offset;
+    gy_offset = _gy_offset;
+    gz_offset = _gz_offset;
+}
+
+//--------------------------------------------//
+// Get the mean values from the sensor
+//--------------------------------------------//
+void ImuPlugin::meanSensors_ ()
+{
+    // Discard first 100 readings
+    for (int i = 0; i < 100; i++)
+    {
+        // read raw accel/gyro measurements from device
+        readIMU();
+        delay(2);
+    }
+    const int bufferSize = 1000;     // Number of readings to average
+    long buff_ax = 0, buff_ay = 0, buff_az = 0, buff_gx = 0, buff_gy = 0, buff_gz = 0;
+
+    for (int i = 0; i < bufferSize; i++)
+    {
+        // read raw accel/gyro measurements from device
+        readIMU();
+
+        buff_ax = buff_ax + accX;
+        buff_ay = buff_ay + accY;
+        buff_az = buff_az + accZ;
+        buff_gx = buff_gx + gyroX;
+        buff_gy = buff_gy + gyroY;
+        buff_gz = buff_gz + gyroZ;
+        delay(2); //Needed so we don't get repeated measures
+    }
+    mean_ax = buff_ax / bufferSize;
+    mean_ay = buff_ay / bufferSize;
+    mean_az = buff_az / bufferSize;
+    mean_gx = buff_gx / bufferSize;
+    mean_gy = buff_gy / bufferSize;
+    mean_gz = buff_gz / bufferSize;
+    log_mean_values();
+}
+
+//--------------------------------------------//
+// Calibrate sensor
+//--------------------------------------------//
+void ImuPlugin::calibrate_ ()
+{
+    ax_offset = ax_offset - mean_ax / acel_deadzone;
+    ay_offset = ay_offset - mean_ay / acel_deadzone;
+    az_offset = az_offset - (16384 - mean_az) / acel_deadzone;
+
+    gx_offset = gx_offset - mean_gx / (giro_deadzone + 1);
+    gy_offset = gy_offset - mean_gy / (giro_deadzone + 1);
+    gz_offset = gz_offset - mean_gz / (giro_deadzone + 1);
+    for (int i = 0; i < 100 && (abs(mean_ax) > acel_deadzone || abs(mean_ay) > acel_deadzone ||
+    abs(16384-mean_az) > acel_deadzone ||
+    abs(mean_gx) > giro_deadzone ||
+    abs(mean_gy) > giro_deadzone ||
+    abs(mean_gz) > giro_deadzone); i++)
+    {
+        mpu.setXAccelOffset(ax_offset);
+        mpu.setYAccelOffset(ay_offset);
+        mpu.setZAccelOffset(az_offset);
+
+        mpu.setXGyroOffset(gx_offset);
+        mpu.setYGyroOffset(gy_offset);
+        mpu.setZGyroOffset(gz_offset);
+
+        // Get the mean values from the sensor
+        meanSensors_();
+        ax_offset = ax_offset - mean_ax / acel_deadzone;
+        ay_offset = ay_offset - mean_ay / acel_deadzone;
+        az_offset = az_offset + (16384 - mean_az) / acel_deadzone;
+        gx_offset = gx_offset - mean_gx / (giro_deadzone + 1);
+        gy_offset = gy_offset - mean_gy / (giro_deadzone + 1);
+        gz_offset = gz_offset - mean_gz / (giro_deadzone + 1);
+        log_mean_values();
+    }
+}
+
+void ImuPlugin::log_mean_values ()
+{
+    Serial.print("mean_ax = ");
+    Serial.print(mean_ax);
+    Serial.print(" mean_ay = ");
+    Serial.print(mean_ay);
+    Serial.print(" mean_az = ");
+    Serial.print(16384 - mean_az);
+
+    Serial.print(" mean_gx = ");
+    Serial.print(mean_gx);
+    Serial.print(" mean_gy = ");
+    Serial.print(mean_gy);
+    Serial.print(" mean_gz = ");
+    Serial.println(mean_gz);
+}
+
+void ImuPlugin::log_offset_values ()
+{
+    Serial.print("ax_offset = ");
+    Serial.print(ax_offset);
+    Serial.print(" ay_offset = ");
+    Serial.print(ay_offset);
+    Serial.print(" az_offset = ");
+    Serial.print(az_offset);
+
+    Serial.print(" gx_offset = ");
+    Serial.print(gx_offset);
+    Serial.print(" gy_offset = ");
+    Serial.print(gy_offset);
+    Serial.print(" gz_offset = ");
+    Serial.println(gz_offset);
 }
 
 void ImuPlugin::cycle ()
 {
     if (is_enabled())
     {
-        read_imu ();
+        read_imu();
     }
 }
 
